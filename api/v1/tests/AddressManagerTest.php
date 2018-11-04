0f53ade3310d0ad97fdb1429d77790bd909e3e72
Vitalii Onanko <v.onanko@8ways.ch>
2018-11-04 11:02:50 +0800

    Install phpunit and create first test

---
+++ api/v1/tests/AddressManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AddressManagerTest extends TestCase
+{
+    public function testCreateAddressObject()
+    {
+        $this->assertEquals(true, false);
+    }
+}
